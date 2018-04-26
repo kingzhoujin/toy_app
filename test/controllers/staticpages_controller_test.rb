aef3e28def874e6606b197dc9a734988244f2979
zhoujin <jin100chow@hotamil.com>
2018-04-26 10:50:54 +0800

    add contact test

--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -1,19 +1,29 @@
 require 'test_helper'
 
 class StaticpagesControllerTest < ActionDispatch::IntegrationTest
+
     test "should get home" do
       get staticpages_home_url
       assert_response :success
       assert_select "title", "Ruby on Rails Tutorial Sample App"
     end
+
     test "should get help" do
       get staticpages_help_url
       assert_response :success
       assert_select "title", "Ruby on Rails Tutorial Sample App"
     end
+
     test "should get about" do
       get staticpages_about_url
       assert_response :success
       assert_select "title", "Ruby on Rails Tutorial Sample App"
    end
+
+   test "should get contact" do
+     get staticpages_contact_url
+     assert_response :success
+     assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+
 end

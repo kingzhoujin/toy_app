ff770524f792a906c6d9f3d744a824b2e9e5b250
zhoujin <jin100chow@hotamil.com>
2018-04-26 14:21:00 +0800

    add signup path instead the user new

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/help',to: 'staticpages#help'
   get '/about',to: 'staticpages#about'
   get '/contact',to: 'staticpages#contact'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

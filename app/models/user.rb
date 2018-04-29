fb3ed5e2f50be4c88affd44f6f15901b649dfe86
zhoujin <jin100chow@hotamil.com>
2018-04-29 08:53:37 +0800

    test from model to test_helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,2 @@
 class User < ApplicationRecord
-  require 'test_helper'
-
-  def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
-  end
-
-  test "should be valid" do
-    assert @user.valid?
-  end
 end
